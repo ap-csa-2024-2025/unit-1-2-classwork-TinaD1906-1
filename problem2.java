e68f73018ea10df0cec8c78237f215d72502256c
TinaD1906 <dotina87@gmail.com>
2024-09-12 10:06:48 -0700

    assignment 1.2

--- problem2.java
+++ problem2.java
@@ -1,9 +1,13 @@
 // Create your class here; don't forget the main method
-public class problem1
+public class problem2
 {
     public static void main(String[] args) 
     {
-        String favfood = "chips";
-        System.out.print ("I like to eat " + favfood + " as well!");
+        String name1 = "bob ";
+        String lastname1 = "bobs ";
+        String name2 = "stuart ";
+        String lastname2 = "stuarts ";
+
+        System.out.print (name1 + lastname1 + name2 + lastname2);
     }
 }
--- problem3.java
+++ problem3.java
@@ -1,1 +1,11 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String name = " Tina";
+        String location = "Westminster.";
+
+        System.out.print ("my name is" + name + ". I live in " + location);
+    }
+}
